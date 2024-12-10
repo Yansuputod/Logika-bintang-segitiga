353dec8335bf4b46c4422878821c48cf7f722ec7
Yansuputod <arta70660@gmail.com>
2024-12-10 20:45:06 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,10 +1,9 @@
 void main(){
   int input = 6;
   String text = "";
-  int stoper = input +1;
 
-  for(int i = 0; i < stoper; i++){
-    for(int j = 0; j < i; j++){
+  for(int i = 1; i <= input; i++){
+    for(int j = 1; j <= i; j++){
       text = text +"* ";
     }
     print(text);
